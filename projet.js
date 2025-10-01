56275169c1646079fa9ba817976702af27a1c6ed
khalil diop <khaliloulahi2003@gmail.com>
2025-10-01 17:37:14 +0100

    fix the rest of functions -trier-titre_annee-

--- projet.js
+++ projet.js
@@ -78,6 +78,38 @@ function trierParAnnee(){
     afficherLivres();
 }
 
+// ==============================Affichier les livere disponible =========
+function afficherDisponibles() {
+    const disponibles = livres.filter(l => l.disponible);
+    if (disponibles.length === 0) {
+        console.log(" Aucun livre disponible actuellement.\n");
+        return;
+    }
+    console.log("\n== Livres disponibles ==");
+    disponibles.forEach(livre => {
+        console.log(`${livre.id_livre} - ${livre.titre} (${livre.annee}) - ${livre.auteur}`);
+    });
+    console.log();
+}
+
+
+
+
+
+// Rechercher un livre par ID==================================
+
+function rechercherLivreParId() {
+    const id = parseInt(prompt("ID du livre: "));
+    const livre = livres.find(l => l.id_livre === id);
+    if (livre) {
+        console.log(` Trouvé: ${livre.id_livre} - ${livre.titre} - ${livre.auteur} - ${livre.disponible ? "Disponible" : "Non disponible"}`);
+    } else {
+        console.log(" Livre introuvable.\n");
+    }
+}
+
+
+
 
 
 
